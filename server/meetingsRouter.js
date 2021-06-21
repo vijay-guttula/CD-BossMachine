70f45d6a5d3fe6f053020c77d4190449b7c79321
Vijay Guttula <ssvijayg@gmail.com>
2021-06-21 19:13:11 +0530

    Meetings router done

--- server/meetingsRouter.js
+++ server/meetingsRouter.js
@@ -10,6 +10,25 @@ const {
   deleteAllFromDatabase,
 } = require('./db');
 
-meetingsRouter.get('/', (req, res, next) => {});
+// Get '/api/meetings' - get an array of all meetings
+meetingsRouter.get('/', (req, res, next) => {
+  res.send(getAllFromDatabase('meetings'));
+});
+
+// POST /api/meetigns - post a new meeting
+meetingsRouter.post('/', (req, res, next) => {
+  const newMeeting = addToDatabase('meetings', createMeeting());
+  res.status(201).send(newMeeting);
+});
+
+// Delete /api/meetings - delete all meetings
+meetingsRouter.delete('/', (req, res, next) => {
+  const deleted = deleteAllFromDatabase('meetings');
+  if (!deleted.length) {
+    res.status(204).send();
+  } else {
+    res.status(500).send();
+  }
+});
 
 module.exports = meetingsRouter;
