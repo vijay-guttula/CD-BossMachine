7dc65ec8d89f1d027eca7c2475ef108aa7d7c8f0
Vijay Guttula <ssvijayg@gmail.com>
2021-06-21 19:43:30 +0530

    Bonus added and Done

--- server/minionRouter.js
+++ server/minionRouter.js
@@ -52,4 +52,50 @@ minionRouter.delete('/:minionId', (req, res, next) => {
   }
 });
 
+/// Work
+
+minionRouter.param('workId', (req, res, next, id) => {
+  const work = getFromDatabaseById('work', id);
+  if (work) {
+    req.work = work;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
+// GET /api/minions/:minionId/work - sends an array of all work
+minionRouter.get('/:minionId/work', (req, res, next) => {
+  const workArray = getAllFromDatabase('work').filter((work) => {
+    return work.minionId === req.params.minionId;
+  });
+  res.send(workArray);
+});
+
+// POST /work - creates and adds new work
+minionRouter.post('/:minionId/work', (req, res, next) => {
+  const workToAdd = req.body;
+  workToAdd.minionId = req.params.minionId;
+  const newWork = addToDatabase('work', workToAdd);
+  res.status(201).send(newWork);
+});
+
+// PUT /work/:workId - updates a current work
+minionRouter.put('/:minionId/work/:workId', (req, res, next) => {
+  if (req.work.minionId !== req.params.minionId) {
+    res.status(400).send();
+  }
+  const workToUpdate = updateInstanceInDatabase('work', req.body);
+  res.send(workToUpdate);
+});
+
+// DELETE /:workId - deletes the work with the given work id
+minionRouter.delete('/:minionId/work/:workId', (req, res, next) => {
+  const isDeleted = deleteFromDatabasebyId('work', req.params.workId);
+  if (isDeleted) {
+    res.status(204).send();
+  }
+  res.status(500).send();
+});
+
 module.exports = minionRouter;
