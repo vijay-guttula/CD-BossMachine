c0670eaa36d8325be74f14d100edf47d038c83a3
Vijay Guttula <ssvijayg@gmail.com>
2021-06-21 19:05:36 +0530

    Ideas Router done

--- server/ideasRouter.js
+++ server/ideasRouter.js
@@ -7,9 +7,48 @@ const {
   updateInstanceInDatabase,
   deleteFromDatabasebyId,
 } = require('./db');
-ideasRouter.param('ideaId', (req, res, next, id) => {});
+
+ideasRouter.param('ideaId', (req, res, next, id) => {
+  const idea = getFromDatabaseById('ideas', id);
+  if (idea) {
+    req.idea = idea;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
 
 // requests
-ideasRouter.get('/');
+// GET /api/ideas - array of all ideas
+ideasRouter.get('/', (req, res, next) => {
+  res.send(getAllFromDatabase('ideas'));
+});
+
+// POST /api/ideas - post a new idea to the db
+ideasRouter.post('/', (req, res, next) => {
+  const newIdea = addToDatabase('ideas', req.body);
+  res.status(201).send(newIdea);
+});
+
+// GET /api/ideas/:ideaId - get an idea of the given id
+ideasRouter.get('/:ideaId', (req, res, next) => {
+  res.status(200).send(req.idea);
+});
+
+// PUT /api/ideas/:ideaId - update the idea with the given id
+ideasRouter.put('/:ideaId', (req, res, next) => {
+  const updatedIdea = updateInstanceInDatabase('ideas', req.body);
+  res.send(updatedIdea);
+});
+
+// DELETE /api/ideas/:ideaId - delete the idea with the id
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+  const isDeleted = deleteFromDatabasebyId('ideas', req.params.ideaId);
+  if (isDeleted) {
+    res.status(204).send();
+  } else {
+    res.status(500).send();
+  }
+});
 
 module.exports = ideasRouter;
--- server/meetingsRouter.js
+++ server/meetingsRouter.js
@@ -1,3 +1,15 @@
 const meetingsRouter = require('express').Router();
 
+const {
+  createMeeting,
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+  deleteAllFromDatabase,
+} = require('./db');
+
+meetingsRouter.get('/', (req, res, next) => {});
+
 module.exports = meetingsRouter;
